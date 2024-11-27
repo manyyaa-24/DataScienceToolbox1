ea546c225e74758e471f4b121c655cde75ed09d2
Manyyaa Chhabra <manyyaachhabra@gmail.com>
2024-11-28 02:35:53 +0530

    Added example R script

---
+++ example_script.R
@@ -0,0 +1,8 @@
+# Create a numeric vector
+numbers <- c(1, 2, 3, 4, 5)
+
+# Calculate the mean of the vector
+mean(numbers)
+
+# Plot the vector
+plot(numbers, type = "o", col = "blue", pch = 19, main = "Sample Plot", xlab = "Index", ylab = "Values")
